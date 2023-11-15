478852181557274b8300a3ab7404db852bd5a26c
ritik <ritikbhatt020@gmail.com>
2023-11-15 21:51:15 +0530

    Added the map to our application..

--- app/src/main/java/com/example/innotech/WelcomePageActivity.kt
+++ app/src/main/java/com/example/innotech/WelcomePageActivity.kt
@@ -1,11 +1,19 @@
 package com.example.innotech
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
 
 class WelcomePageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome_page)
+
+        val btnAmbulance = findViewById<AppCompatButton>(R.id.btn_ambulance)
+        btnAmbulance.setOnClickListener {
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

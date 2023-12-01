d8cbe485afa22c245f2006de2400ed59f2b80c73
ritik <ritikbhatt020@gmail.com>
2023-12-01 09:07:11 +0530

    Got the changes done

---
+++ app/src/main/java/com/example/innotech/PharmacyListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.innotech
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PharmacyListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pharmacy_list)
+    }
+}
